99d479597bb4fef8401b1a686ee1731abb268d3c
Leonardo Soares <leonaardo.soaares@gmail.com>
2020-07-20 06:18:40 -0300

    Criando função da  cobrinha

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext('2d')
 let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
-function criar() {
+function criarEspaço() {
     context.fillStyle = "Blue";
     context.fillRect(0, 0, 16  * box, 16 * box);
 }
- criar(); 
+
+function criarCobra{
+    for(var i = 0; i < snake.length: i++) {
+        context.fillStyle = "white"
+        context.fillRect(snake[i].x, snake[i].y box ,box);
+
+    }
+}
+
+ criarEspaço();
+ criarCobra();
