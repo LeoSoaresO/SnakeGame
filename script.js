a5f75072cea9004e455081acf6cedf5e1c0eeb35
Leonardo Soares <leonaardo.soaares@gmail.com>
2020-07-20 07:50:25 -0300

    Função de Morrer

--- script.js
+++ script.js
@@ -39,11 +39,19 @@ function update(event) {
 }
 
 function iniciarJogo() {
+
     if(snake[0].x > 15 * box && direcao == "right") snake[0].x = 0;
     if(snake[0].x < 0 * box && direcao == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direcao == "down") snake[0].y = 0;
     if(snake[0].y < 0 * box && direcao == "up") snake[0].y = 16 * box;
  
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert("Game over")
+        }
+    }
+
     criarEspaço();
     criarCobra();
     criarComida();
